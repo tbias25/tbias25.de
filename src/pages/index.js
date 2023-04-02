ade43effcdc7cff3da58b181f9e666aa2b1860db
Tobias <55360690+tbias25@users.noreply.github.com>
2023-04-02 15:06:12 +0200

    some updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,9 @@ import Link from "next/link";
 
 export default function Home() {
   const handleScroll = (e) => {
-    // first prevent the default behavior
     e.preventDefault();
-    // get the href and remove everything before the hash (#)
     const href = e.currentTarget.href;
     const targetId = href.replace(/.*\#/, "");
-    // get the element by id and use scrollIntoView
     const elem = document.getElementById(targetId);
     elem?.scrollIntoView({
       behavior: "smooth",
